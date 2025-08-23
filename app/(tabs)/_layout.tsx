1a4d5c1ec2899a55847f424b380ed270c7c75cc1
Asif Kabani <asifkabani@gmail.com>
2025-08-23 15:13:17 -0500

    chore: Modify navigation to return tab screens from configuration

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,9 +2,16 @@ import Ionicons from "@expo/vector-icons/Ionicons";
 import { Tabs } from "expo-router";
 
 export default function TabLayout() {
+  const tabScreens = [
+    { name: "index", title: "Home", icon: "home" },
+    { name: "pets", title: "Pets", icon: "paw" },
+    { name: "settings", title: "Settings", icon: "cog" },
+  ];
+
   return (
     <Tabs
       screenOptions={{
+        animation: "shift",
         headerShown: false,
         tabBarActiveTintColor: "#ffd33d",
         tabBarStyle: {
@@ -12,45 +19,23 @@ export default function TabLayout() {
         },
       }}
     >
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: "Home",
-          tabBarIcon: ({ color, focused }) => (
-            <Ionicons
-              name={focused ? "home-sharp" : "home-outline"}
-              color={color}
-              size={24}
-            />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="pets"
-        options={{
-          title: "Pets",
-          tabBarIcon: ({ color, focused }) => (
-            <Ionicons
-              name={focused ? "paw-sharp" : "paw-outline"}
-              color={color}
-              size={24}
-            />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="settings"
-        options={{
-          title: "Settings",
-          tabBarIcon: ({ color, focused }) => (
-            <Ionicons
-              name={focused ? "cog-sharp" : "cog-outline"}
-              color={color}
-              size={24}
-            />
-          ),
-        }}
-      />
+      {tabScreens.map((tab, index) => {
+        const iconName = (focused: boolean): string =>
+          focused ? `${tab.icon}-sharp` : `${tab.icon}-outline`;
+
+        return (
+          <Tabs.Screen
+            key={`${tab.name}-${index}`}
+            name={tab.name}
+            options={{
+              title: `${tab.title}`,
+              tabBarIcon: ({ color, focused }) => (
+                <Ionicons name={iconName(focused)} color={color} size={24} />
+              ),
+            }}
+          />
+        );
+      })}
     </Tabs>
   );
 }
